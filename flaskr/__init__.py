e9d7b9586be76f41ab2bf4bd1a6d2aff910ca6b2
asychev <anton.sychev@fccho-moscow.ru>
2024-09-06 16:51:45 +0300

    база данных, форма, таблица

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,13 +1,24 @@
 import os
 from markupsafe import escape
 import functools
-
+from sqlalchemy.orm import Session
+from sqlalchemy import select
 from flask import (
     Blueprint, Flask, flash, g, redirect, render_template, request, session, url_for
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 
-import flaskr.db as db
+from flaskr.db import init_app, engine, User, Base, Sex
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField
+from wtforms.validators import DataRequired
+
+
+class MyForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
+    sex = SelectField('Sex', choices={(1, 'Male'), (2, 'Female')})
+
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
@@ -33,43 +44,22 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    @app.route('/', methods=['POST', 'GET'])
-    def index():
-        database = db.get_db()
-        keys = ('early_toxicity', 'ray_therapy', 'ray_therapy_comp', 'sex', 'event', 'main_request', 'deauville_score', 'anat_request', 'groups', 'protocol', 'stage', 'histologic', 'b_symptoms', 'region', 'e_impact',
-                'bm_damage_myelogram', 'bm_damage_kt', 'request_pat', 'start_therapy_date', 'end_therapy_date', 'event_date', 'last_date', 'birth_date', 'leykocytes', 'hemoglobin', 'trombocytes', 'ldh', 'ebv', 'esr', 'init_val', 'tumor_val')
-        data = ()
-        str_insert = ()
-        for key in keys:
-            get_data = request.form.get(key)
-            if get_data is None:
-                get_data = 'null'
-            data += (str(get_data),)
-            str_insert += ('?', )
-        if request.method == 'POST':
-            str_val = ', '
-            keys = str("(" + str_val.join(keys) + ")")
-            str_insert = str("(" + str_val.join(str_insert) + ")")
-            print(keys)
-            print(data)
-            database.execute(
-                "INSERT INTO person " + keys + " VALUES" + str_insert, data)
-            database.commit()
-            return f"{data}"
-        return render_template('form.html')
+    @app.route('/submit', methods=['GET', 'POST'])
+    def submit():
+        form = MyForm()
+        if form.validate_on_submit():
+            with Session(engine) as session:
+                person = User(name=form.name.data, sex=form.sex.data)
+                session.add(person)
+                session.commit()
+            return redirect('/select')
+        return render_template('submit.html', form=form)
 
-    @app.route('/test')
-    def test():
-        database = db.get_db()
-        users: list[tuple] = database.execute(
-            "SELECT * FROM person").fetchall()
-        print(users)
-        for row in users:
-            print(f"User ID: {row[0]}")
-        if users is None:
-            return "None"
-        else:
-            return f"{users[0][1]}!"
+    @app.route('/select')
+    def select_name():
+        session = Session(engine)
+        stmt = select(User).join(Sex.sex)
+        return render_template('success.html', persons=session.scalars(stmt))
 
     def valid_login(username, password):
         if username == password:
@@ -80,7 +70,7 @@ def create_app(test_config=None):
     def log_the_user_in(username):
         redirect('/user/<' + username + '>')
 
-    @ app.route('/login', methods=['POST', 'GET'])
+    @app.route('/login', methods=['POST', 'GET'])
     def login():
         error = None
         if request.method == 'POST':
@@ -97,15 +87,16 @@ def create_app(test_config=None):
             </form>
         '''
 
-    @ app.route('/user/<username>')
+    @app.route('/user/<username>')
     def profile(username):
         return f'{username}\'s profile'
 
     with app.test_request_context():
-        print(url_for('index'))
+        # print(url_for('index'))
         print(url_for('login'))
         print(url_for('login', next='/'))
         print(url_for('profile', username='John Doe'))
 
-    db.init_app(app)
+    init_app(app)
+    Base.metadata.create_all(engine)
     return app
--- flaskr/db.py
+++ flaskr/db.py
@@ -1,41 +1,56 @@
-import sqlite3
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, ForeignKey, String
+from sqlalchemy_utils import database_exists, create_database
+from sqlalchemy.orm import Session, DeclarativeBase, Mapped, mapped_column, relationship
 
 import click
 from flask import current_app, g
 
-
-def get_db():
-    if 'db' not in g:
-        g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
-            detect_types=sqlite3.PARSE_DECLTYPES
-        )
-        g.db.row_factory = sqlite3.Row
-
-    return g.db
+from typing import List, Optional
 
 
-def close_db(e=None):
-    db = g.pop('db', None)
-
-    if db is not None:
-        db.close()
+class Base(DeclarativeBase):
+    pass
 
 
-def init_db():
-    db = get_db()
+class User(Base):
+    __tablename__ = "user_account"
 
-    with current_app.open_resource('schema.sql') as f:
-        db.executescript(f.read().decode('utf8'))
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30))
+    sex: Mapped[int] = mapped_column()
+
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
+
+
+class Sex(Base):
+    __tablename__ = "sex"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    sex: Mapped[str] = mapped_column(String(30))
+
+
+engine = create_engine('sqlite:///database.db', echo=True)
+if not database_exists(engine.url):
+    create_database(engine.url)
+print(database_exists(engine.url))
 
 
 @click.command('init-db')
 def init_db_command():
-    """Clear the existing data and create new tables."""
-    init_db()
-    click.echo('Initialized the database.')
+    # """Clear the existing data and create new tables."""
+    # init_db()
+    Base.metadata.create_all(engine)
+    with Session(engine) as session:
+        male = Sex(id=1, sex='Male')
+        female = Sex(id=2, sex='Female')
+        session.add(male, female)
+        session.commit()
+        click.echo('Initialized the database.')
 
 
 def init_app(app):
-    app.teardown_appcontext(close_db)
+    # app.teardown_appcontext(close_db)
     app.cli.add_command(init_db_command)
